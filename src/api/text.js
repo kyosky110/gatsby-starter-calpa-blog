5b8643594a3042a3d9899934bbeb30dd26645991
HuangNan <493203254@qq.com>
2019-01-02 16:36:23 +0800

    ff

--- src/api/text.js
+++ src/api/text.js
@@ -1,11 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies, import/order, import/no-self-import */
-const Remarkable = require('remarkable');
-const { escapeHtml, replaceEntities } = require('remarkable/lib/common/utils');
-const { getGalleryImage } = require('./images');
-const hljs = require('highlight.js/lib/highlight');
+const Remarkable = require("remarkable");
+const { escapeHtml, replaceEntities } = require("remarkable/lib/common/utils");
+const { getGalleryImage } = require("./images");
+const hljs = require("highlight.js/lib/highlight");
 
-const beautifyCode = (code, language = 'javascript') => {
-  ['javascript', 'bash'].forEach((langName) => {
+const beautifyCode = (code, language = "javascript") => {
+  ["javascript", "bash"].forEach(langName => {
     // Using require() here because import() support hasn't landed in Webpack yet
     // eslint-disable-next-line global-require, import/no-dynamic-require
     const langModule = require(`highlight.js/lib/languages/${langName}`);
@@ -19,7 +19,7 @@ const beautifyCode = (code, language = 'javascript') => {
   return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`;
 };
 
-const extractId = (text = '') => {
+const extractId = (text = "") => {
   let id;
   const link = text.match(/<a.*>(.*)<\/a>/);
   if (link) {
@@ -28,18 +28,18 @@ const extractId = (text = '') => {
     // Extract Chinese and English wordings
     const temp = text.match(/[\u4e00-\u9fa5\S]+/g);
     if (temp) {
-      id = temp.join('');
+      id = temp.join("");
     }
   }
   return id;
 };
 
-const getContent = async (mdFile) => {
+const getContent = async mdFile => {
   const toc = [];
   const md = new Remarkable({
     highlight(str, lang) {
       return beautifyCode(str, lang);
-    },
+    }
   });
 
   // Add image to fancybox
@@ -54,20 +54,21 @@ const getContent = async (mdFile) => {
     return `<h${tokens[idx].hLevel} id=${id}>`;
   };
 
-  md.renderer.rules.table_open = () => '<div class="table-responsive"><table class="table table-striped">';
+  md.renderer.rules.table_open = () =>
+    '<div class="table-responsive"><table class="table table-striped">';
 
-  md.renderer.rules.table_close = () => '</table></div>';
+  md.renderer.rules.table_close = () => "</table></div>";
 
   md.renderer.rules.blockquote_open = () => '<blockquote class="blockquote">';
 
   md.renderer.rules.link_open = (tokens, idx) => {
     const title = tokens[idx].title
       ? ` title="${escapeHtml(replaceEntities(tokens[idx].title))}"`
-      : '';
+      : "";
     const href = escapeHtml(tokens[idx].href);
-    const isExternal = href.charAt(0) !== '/';
-    const target = isExternal ? 'target="_blank"' : '';
-    const rel = isExternal ? 'rel="external nofollow noopener"' : '';
+    const isExternal = href.charAt(0) !== "/";
+    const target = isExternal ? 'target="_blank"' : "";
+    const rel = isExternal ? 'rel="external nofollow noopener"' : "";
     return `<a ${rel} href="${href}"${title}${target}>`;
   };
 
@@ -75,12 +76,12 @@ const getContent = async (mdFile) => {
   return { html, toc };
 };
 
-const getFirstParagraph = (content) => {
+const getFirstParagraph = content => {
   const introduction = content.match(/[\n]+(.*)[\n]/);
   return introduction[1].substring(0, 140);
 };
 
 module.exports = {
   getContent,
-  getFirstParagraph,
+  getFirstParagraph
 };
